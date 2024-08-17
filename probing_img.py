29fe45170c8cc21e835ce5ba3cbf7efb489126db
zualmerul <135142064+l-pommeret@users.noreply.github.com>
2024-08-17 17:55:51 +0200

    txt

--- probing_img.py
+++ probing_img.py
@@ -63,13 +63,9 @@ def get_probe_points(model):
             f'layer{layer}_attn',
             f'layer{layer}_post_attn',
             f'layer{layer}_pre_ffn_norm',
+            f'layer{layer}_mlp_fc',
+            f'layer{layer}_mlp_proj'
         ])
-        
-        # Add probe points for each MLP layer
-        mlp_layers = model.transformer.h[layer].mlp.net
-        for mlp_idx, _ in enumerate(mlp_layers):
-            if mlp_idx % 2 == 0:  # Only probe after activation functions
-                probe_points.append(f'layer{layer}_mlp{mlp_idx//2}')
     
     probe_points.extend(['final_ln', 'lm_head'])
     return probe_points
@@ -105,27 +101,27 @@ def extract_activations_all_points(model, text_games, device, batch_size=4, chun
                     x = model.transformer.drop(embedding)
                     
                     # Process all layers
-                    for layer_idx, layer in enumerate(model.transformer.h):
-                        ln1_out = layer.ln_1(x)
+                    for layer_idx, block in enumerate(model.transformer.h):
+                        ln1_out = block.ln_1(x)
                         chunk_activations[f'layer{layer_idx}_pre_attn_norm'].append(ln1_out.cpu())
                         
-                        attn_output, _ = layer.attn(ln1_out)
+                        attn_output = block.attn(ln1_out)
                         chunk_activations[f'layer{layer_idx}_attn'].append(attn_output.cpu())
                         
                         x = x + attn_output
                         chunk_activations[f'layer{layer_idx}_post_attn'].append(x.cpu())
                         
-                        ln2_out = layer.ln_2(x)
+                        ln2_out = block.ln_2(x)
                         chunk_activations[f'layer{layer_idx}_pre_ffn_norm'].append(ln2_out.cpu())
                         
-                        # Process MLP layers
-                        mlp_input = ln2_out
-                        for mlp_idx, mlp_layer in enumerate(layer.mlp.net):
-                            mlp_input = mlp_layer(mlp_input)
-                            if mlp_idx % 2 == 1:  # After activation function
-                                chunk_activations[f'layer{layer_idx}_mlp{mlp_idx//2}'].append(mlp_input.cpu())
+                        # MLP layers
+                        mlp_fc = block.mlp.c_fc(ln2_out)
+                        chunk_activations[f'layer{layer_idx}_mlp_fc'].append(mlp_fc.cpu())
                         
-                        x = x + mlp_input
+                        mlp_output = block.mlp(ln2_out)
+                        chunk_activations[f'layer{layer_idx}_mlp_proj'].append(mlp_output.cpu())
+                        
+                        x = x + mlp_output
                     
                     # Final layer norm
                     x = model.transformer.ln_f(x)
@@ -179,7 +175,7 @@ def train_and_evaluate_probing_classifiers(activations, labels):
     
     for position in range(9):
         y = labels[:, position]
-        base_clf = LogisticRegression(max_iter=5000)
+        base_clf = LogisticRegression(max_iter=500)
         clf = OneVsRestClassifier(base_clf)
         
         kf = KFold(n_splits=n_splits, shuffle=True, random_state=42)
