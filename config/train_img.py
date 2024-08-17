c242eebe903516c6ba02c1694196dddb54d1c784
zualmerul <135142064+l-pommeret@users.noreply.github.com>
2024-08-17 15:29:59 +0200

    change config

--- config/train_img.py
+++ config/train_img.py
@@ -18,8 +18,8 @@ block_size = 144  # context of up to 1023 tokens (because dataset block size is 
 
 # baby GPT model :)
 n_layer = 2
-n_head = 1
-n_embd = 512
+n_head = 4
+n_embd = 256
 dropout = 0.0
 
 learning_rate = 3e-4
