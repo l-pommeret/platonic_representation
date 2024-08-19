2bdbd4ba3a4d420de74cd06d04def8aef5f8c49e
zualmerul <135142064+l-pommeret@users.noreply.github.com>
2024-08-19 19:48:16 +0200

    change batch

--- config/train_txt.py
+++ config/train_txt.py
@@ -13,13 +13,13 @@ wandb_run_name = "TXT_1_layer"
 
 dataset = "txt"
 gradient_accumulation_steps = 1
-batch_size = 512
+batch_size = 2048
 block_size = 35  # context of up to 1023 tokens (because dataset block size is 1024)
 
 # baby GPT model :)
 n_layer = 2
 n_head = 2
-n_embd = 256
+n_embd = 128
 dropout = 0.0
 
 learning_rate = 3e-4
