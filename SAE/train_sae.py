dca32d232078771cc42c8f36d7dd0a97c15dd653
zualmerul <135142064+l-pommeret@users.noreply.github.com>
2024-09-11 12:27:29 +0200

    model import bug

--- SAE/train_sae.py
+++ SAE/train_sae.py
@@ -2,6 +2,8 @@ import torch
 import pickle
 import os
 import random
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from model import GPTConfig, GPT
 
 # Chargement des données d'encodage
